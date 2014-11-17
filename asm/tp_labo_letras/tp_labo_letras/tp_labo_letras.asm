5dbc906cd30387e1f1e99146ff828024f19813c5
Agustin Santiago <agus.santiago@gmail.com>
2014-11-17 01:12:03 -0300

    Agrego el assembly del codigo en C que ya estaba. Resta implementar el offsetColumna (para ir desplazando el texto) y el dibujo del pixel en si (no recuerdo puerto y bit para hacerlo)

--- asm/tp_labo_letras/tp_labo_letras/tp_labo_letras.asm
+++ asm/tp_labo_letras/tp_labo_letras/tp_labo_letras.asm
@@ -7,8 +7,8 @@
 
 cadena:
 	.db 2, 0x00				; len(text)
-	.dw letra_espacio
 	.dw letra_a
+	.dw letra_b
 
 letra_espacio:
 	.db 0b00000000, 0b00000000, 0b00000000, 0b00000000, 0b00000000, 0b00000000, 0b00000000, 0b00000000
@@ -19,42 +19,7 @@ letra_b:
 letra_c:
 	.db 0b00111110, 0b01000000, 0b01000000, 0b01000000, 0b01000000, 0b01000000, 0b01000000, 0b00111110
 
-main:
-	; Se shiftea uno hacia la izquierda para multiplicar por 2
-	; El direccionamiento de las etiquetas es a Word, o sea a 2 bytes
-	; En cambio, lpm utiliza direccionamiento a byte
-
-	ldi ZH, high(cadena<<1)
-	ldi ZL, low(cadena<<1)
-	
-	lpm r0, Z+				; len(text)
-	inc ZL					; padding
-
-	; for
-
-		lpm r28, Z+				; r26 <- LOW(*letra_a)
-		lpm r29, Z+				; r27 <- HIGH(*letra_a)
-
-		mov r26, r30			; r26 <- LOW(dir_actual)
-		mov r27, r31			; r27 <- HIGH(dir_actual)
-
-		mov r30, r28			; LOW(Z) = LOW(*letra_a)
-		mov r31, r29			; HIGH(Z) = HIGH(*letra_a)
-		lsl r30					; LOW(*letra_a)<<1
-		lsl r31					; HIGH(*letra_a)<<1
-
-		; for row in letra
-			lpm r1, Z				; r1 = letra_a[0]
-
-		mov r30, r26
-		mov r31, r27
-
-	jmp main
-
 /*
-
-global lenCadena = INT;
-
 function dibujar (fila, columna, offsetColumna) {
 	
 	dir(*letra) = primer_ptr + floor(offsetColumna / 8) + floor(columna / 8)
@@ -62,5 +27,53 @@ function dibujar (fila, columna, offsetColumna) {
 	bit = byte_letra[columna mod 8 + offsetColumna mod 8]
 
 }
-
 */
+
+//r1 = fila, r2 = columna, r3 = offsetColumna
+dibujar:
+	; Se shiftea uno hacia la izquierda para multiplicar por 2
+	; El direccionamiento de las etiquetas es a Word, o sea a 2 bytes
+	; En cambio, lpm utiliza direccionamiento a byte
+
+	;Z = &cadena
+	ldi ZH, high(cadena<<1)
+	ldi ZL, low(cadena<<1)
+	
+	//r0 = count(letras)
+	//Z = &(letras[0])
+	lpm r4, Z+				; r4 = count(letras)
+	inc ZL					; padding
+
+	//TODO offsetColumna
+	//Z += floor(offsetColumna / 8) + floor(columna / 8)
+	//Z = &(&letra_a_imprimir)
+		//r5 = 2*floor(columna/8) ;cada registro ocupa 2 bytes y cada letra tiene 8 pixeles de ancho
+		mov r5, r2
+		lsr r5
+		lsr r5
+		//Z += 2*floor(columna/8)
+		clr r0
+		add r30, r5
+		adc r31, r0
+
+	//r28:r29 = &letra_a_imprimir
+		lpm r28, Z+				; r28 <- LOW(&letra_a_imprimir)
+		lpm r29, Z+				; r29 <- HIGH(&letra_a_imprimir)
+		//r28:r29 = &letra_a_imprimir ;direccionamiento a word => direccionamiento a byte
+		lsl r28
+		lsl r29
+
+	//Z = &letra_a_imprimir
+	mov r30,r28
+	mov r31,r29
+
+	//Z = &fila_letra_a_imprimir = &letra_a_imprimir + fila
+	clr r0
+	add r30,r1
+	adc r31,r0
+
+	//r6 = fila_letra_a_imprimir
+	lpm r6, Z
+
+	//TODO DIBUJAR POSTA
+	//bit = r6[columna mod 8 + offsetColumna mod 8]
